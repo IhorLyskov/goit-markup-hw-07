58597f344b17bfd7cdab1c328579ad87f030ac44
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-05-12 01:42:16 +0300

    some

--- js/debug.js
+++ js/debug.js
@@ -1,6 +1,5 @@
 (() => {
   document.querySelector('[js-modal-form]').addEventListener('submit', e => {
-    console.log('debug found');
     e.preventDefault();
 
     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
