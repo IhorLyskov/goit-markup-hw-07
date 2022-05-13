996db8ae0a82468eaa82a5f73f345f5153bf767d
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-05-14 00:36:12 +0300

    mixin

--- js/debug.js
+++ js/debug.js
@@ -1,12 +1,9 @@
 (() => {
-  console.log('startDebug');
-
   document.querySelector('[js-modal-form]').addEventListener('submit', e => {
     e.preventDefault();
 
     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
 
     e.currentTarget.reset();
-    console.log('endDebug');
   });
 })();
